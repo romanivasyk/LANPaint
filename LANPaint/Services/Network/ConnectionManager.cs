25c914c32e08f8109fa6df2c8a501886daca969c
Roman Ivasyk <Kysavi7@gmail.com>
2021-02-07 03:08:37 +0200

    Added handling of disconnects. Still working on udpBroadcast initialization...

--- LANPaint/Services/Network/ConnectionManager.cs
+++ LANPaint/Services/Network/ConnectionManager.cs
@@ -8,6 +8,18 @@ namespace LANPaint.Services.Network
 {
     public class NetworkInterfaceHelper
     {
+        public bool IsAnyNetworkAvailable { get; private set; }
+
+        public NetworkInterfaceHelper()
+        {
+            NetworkChange.NetworkAvailabilityChanged += NetworkAvailabilityChangedHandler;
+        }
+
+        private void NetworkAvailabilityChangedHandler(object sender, NetworkAvailabilityEventArgs e)
+        {
+            IsAnyNetworkAvailable = NetworkInterface.GetIsNetworkAvailable();
+        }
+
         public IEnumerable<NetworkInterface> GetIPv4Interfaces()
         {
             return NetworkInterface.GetAllNetworkInterfaces().Where(ni =>
@@ -20,7 +32,7 @@ namespace LANPaint.Services.Network
 
         public bool IsReadyToUse(NetworkInterface ni) => ni.OperationalStatus == OperationalStatus.Up;
 
-        public IPAddress GetIpAddress(NetworkInterface ni) => ni.GetIPProperties().UnicastAddresses
+        public IPAddress GetIpAddress(NetworkInterface ni) => ni?.GetIPProperties().UnicastAddresses
             .First(information => information.Address.AddressFamily == AddressFamily.InterNetwork).Address;
     }
 }
