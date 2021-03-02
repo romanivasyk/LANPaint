e3a605f7046a657df4ee047e86ff0f4682a0dd8c
Roman Ivasyk <Kysavi7@gmail.com>
2021-03-02 22:51:03 +0200

    Moved PaintViewModel instantiation to App class.

--- LANPaint/App.xaml.cs
+++ LANPaint/App.xaml.cs
@@ -1,11 +1,36 @@
 ﻿using System.Windows;
+using LANPaint.Dialogs.FrameworkDialogs;
+using LANPaint.Dialogs.Service;
+using LANPaint.Services.Broadcast;
+using LANPaint.Services.Broadcast.UDP.Factories;
+using LANPaint.Services.IO;
+using LANPaint.ViewModels;
+using LANPaint.Views;
 
 namespace LANPaint
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private PaintViewModel _paintDataContext;
+        
+        //TODO: Move this stuff to some kind of Setup or ApplicationBootstrapper with DI container.
+        private void OnStartupHandler(object sender, StartupEventArgs e)
+        {
+            var broadcastFactory = new ChainerFactory(16384);
+            var broadcastService = new BroadcastService(broadcastFactory);
+            var frameworkDialogFactory = new DefaultFrameworkDialogFactory();
+            var dialogService = new DefaultDialogService(frameworkDialogFactory);
+            var fileService = new DefaultFileService();
+
+            _paintDataContext = new PaintViewModel(broadcastService, dialogService, fileService);
+
+            var paint = new Paint {DataContext = _paintDataContext, };
+            paint.Show();
+        }
+
+        private void OnExitHandler(object sender, ExitEventArgs e)
+        {
+            _paintDataContext.Dispose();
+        }
     }
 }
--- LANPaint/Views/Paint.xaml.cs
+++ LANPaint/Views/Paint.xaml.cs
@@ -1,12 +1,4 @@
-﻿using LANPaint.Dialogs.FrameworkDialogs;
-using LANPaint.Dialogs.Service;
-using LANPaint.Services.Broadcast.UDP.Factories;
-using LANPaint.ViewModels;
-using System;
-using System.Windows;
-using System.Windows.Media;
-using LANPaint.Services.Broadcast;
-using LANPaint.Services.IO;
+﻿using System.Windows;
 
 namespace LANPaint.Views
 {
@@ -15,19 +7,6 @@ namespace LANPaint.Views
         public Paint()
         {
             InitializeComponent();
-            var broadcastFactory = new ChainerFactory(16384);
-            var broadcastService = new BroadcastService(broadcastFactory);
-            var frameworkDialogFactory = new DefaultFrameworkDialogFactory();
-            var dialogService = new DefaultDialogService(frameworkDialogFactory);
-            var fileService = new DefaultFileService();
-
-            var context = new PaintViewModel(broadcastService, dialogService, fileService);
-            DataContext = context;
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            if (DataContext is IDisposable disposableContext) disposableContext.Dispose();
         }
     }
 }
