c19efef87f6d03c38489b4b4d6b0ea5ab0359231
Roman Ivasyk <Kysavi7@gmail.com>
2021-03-20 02:54:52 +0200

    Added ToolTip for Settings dialog.

--- LANPaint/Views/Settings.xaml.cs
+++ LANPaint/Views/Settings.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace LANPaint.Views
@@ -9,10 +11,19 @@ namespace LANPaint.Views
         {
             InitializeComponent();
         }
-        
+
         private void Port_PreventInputNonNumbers(object sender, TextCompositionEventArgs e) =>
             e.Handled = !ushort.TryParse(e.Text, out var parsed);
 
-        private void Port_PreventInputSpaces(object sender, KeyEventArgs e) => e.Handled = e.Key == Key.Space;
+        private void Port_PreventInputSpaces(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) e.Handled = true;
+            else if (e.Key == Key.Enter && OkButton.IsEnabled)
+            {
+                var peer = new ButtonAutomationPeer(OkButton);
+                var invokeProvider = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
+                invokeProvider?.Invoke();
+            }
+        }
     }
 }
