03ed61a0363a8bcd4fc2a14aa8503c8bbb04e467
Roman Ivasyk <Kysavi7@gmail.com>
2021-04-02 00:24:33 +0300

    ClearInstruction covered with unit tests.

---
+++ LANPaint.UnitTests/DrawingInstructions/ClearInstructionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LANPaint.DrawingInstructions;
+using LANPaint.DrawingInstructions.Interfaces;
+using Moq;
+using Xunit;
+
+namespace LANPaint.UnitTests.DrawingInstructions
+{
+    public class ClearInstructionTests
+    {
+        [Fact]
+        public void ExecuteDrawingInstructionPassNull()
+        {
+            var instruction = new ClearInstruction();
+            Assert.Throws<ArgumentNullException>(() => instruction.ExecuteDrawingInstruction(null));
+        }
+
+        [Fact]
+        public void ExecuteDrawingInstruction()
+        {
+            var repositoryMock = new Mock<IDrawingInstructionRepository>();
+            var instruction = new ClearInstruction();
+            
+            instruction.ExecuteDrawingInstruction(repositoryMock.Object);
+            
+            repositoryMock.Verify(mock=>mock.Clear(), Times.Once);
+        }
+    }
+}
--- LANPaint/DrawingInstructions/ClearInstruction.cs
+++ LANPaint/DrawingInstructions/ClearInstruction.cs
@@ -8,6 +8,7 @@ namespace LANPaint.DrawingInstructions
     {
         public void ExecuteDrawingInstruction(IDrawingInstructionRepository instructionRepository)
         {
+            if (instructionRepository == null) throw new ArgumentNullException(nameof(instructionRepository));
             instructionRepository.Clear();
         }
     }
