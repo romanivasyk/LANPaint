bc148510b9f61841399f19bd9d6e0918973d6e12
Roman Ivasyk <Kysavi7@gmail.com>
2021-04-17 02:23:12 +0300

    NetworkServiceFactory covered with unit tests.

---
+++ LANPaint.UnitTests/Services/Network/NetworkServiceFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using LANPaint.Services.Network;
+using LANPaint.Services.Network.Utilities;
+using LANPaint.Services.Network.Watchers;
+using Xunit;
+
+namespace LANPaint.UnitTests.Services.Network
+{
+    public class NetworkServiceFactoryTests
+    {
+        private readonly NetworkServiceFactory factory = new NetworkServiceFactory();
+        
+        [Fact]
+        public void GetNetworkWatcher()
+        {
+            Assert.Equal(factory.CreateWatcher().GetType(), typeof(NetworkWatcher));
+        }
+
+        [Fact]
+        public void GetNetworkUtility()
+        {
+            Assert.Equal(factory.CreateUtility().GetType(), typeof(NetworkUtility));
+        }
+    }
+}
